5891052fa945f15836dae4c06f6a341ba29d4442
Sergei Danilov <sergeidanilov@gmail.com>
2024-07-22 16:28:46 -0700

    fix: make establishing cifs ssh tunnel more reliable
    restart both autossh and ssh each time on tunnel start since autossh not always restarts ssh if we kill just it.
    check in the end that tunnel was successfully established and report it either as sucess of failure

--- cmd/cifs_ssh_tunnel.sh
+++ cmd/cifs_ssh_tunnel.sh
@@ -19,14 +19,40 @@ check_env_variable SSH_USER $SSH_USER
 check_env_variable SSH_HOST $SSH_HOST
 check_env_variable PRIVATE_KEY $PRIVATE_KEY
 
-if ss -tln | grep -qw "127.0.0.1:$local_port" ; then
-  # kill previous ssh tunnel. autossh will automatically start new tunnel
-  fuser -k $local_port/tcp 2>/dev/null
-else
-  # start new ssh tunnel if nothing is listening
-  AUTOSSH_PORT=0
-  AUTOSSH_GATETIME=0
-  # Reduce noise from ssh tunnel errors/restarts etc 
-  AUTOSSH_LOGLEVEL=0
-  autossh -fNT -o ExitOnForwardFailure=yes -o ServerAliveInterval=10 -o ServerAliveCountMax=3 -i $PRIVATE_KEY -p $SSH_PORT -L $local_port:127.0.0.1:$remote_cifs_port $SSH_USER@$SSH_HOST
-fi
+# restart both autossh and ssh because sometimes autossh does not restart ssh process when its killed
+autossh_pids=$(ps aux | grep "autossh" | grep "$local_port:127.0.0.1:" |  grep -v grep | awk '{print $2}')
+IFS=$'\n'
+for autossh_pid in $autossh_pids 
+do
+  autossh_pid=$(echo $autossh_pid | tr -d ' ')
+  group_id=$(ps -o pgid= $autossh_pid | tr -d ' ')
+  if [ -n "$group_id" ]; then
+    do_log "kill autossh $group_id and child ssh process"
+    kill -9 -$group_id
+  fi
+done
+
+AUTOSSH_PORT=0
+AUTOSSH_GATETIME=0
+# Reduce noise from ssh tunnel errors/restarts etc 
+AUTOSSH_LOGLEVEL=0
+autossh -fNT -o ExitOnForwardFailure=yes -o ServerAliveInterval=10 -o ServerAliveCountMax=3 -i $PRIVATE_KEY -p $SSH_PORT -L $local_port:127.0.0.1:$remote_cifs_port $SSH_USER@$SSH_HOST
+
+# check ssh is listening on port. tunnel takes some time to come up
+start_time=$(date +%s)
+max_wait_time_sec=20
+while true; do
+  ssh_tunnel_pid=$(fuser -n tcp $local_port 2>/dev/null| awk -F":" '{print $1}')
+  if [ -n "$ssh_tunnel_pid" ]; then
+    do_log "ssh tunnel is listening on port $local_port after $elapsed_time seconds"
+    exit 0
+  else
+    current_time=$(date +%s)
+    elapsed_time=$((current_time - start_time))
+    if [ $elapsed_time -ge $max_wait_time_sec ]; then
+      do_log "Error: ssh tunnel failed to start after $elapsed_time seconds" >&2
+      exit 1
+    fi
+    sleep 0.5
+  fi
+done
