cef253b0b2b341340fa6882f1f8c1b7f93f37279
Sergei Danilov <sergeidanilov@gmail.com>
2025-10-12 12:30:02 -0700

    feat: expose local cifs port to be configurable

--- cmd/cifs_ssh_tunnel.sh
+++ cmd/cifs_ssh_tunnel.sh
@@ -11,16 +11,16 @@ function check_env_variable() {
         exit 1  # Exit with failure code
     fi
 }
-local_port=4445
-remote_cifs_port=445
+remote_cifs_port=4445
 
 check_env_variable SSH_PORT $SSH_PORT
 check_env_variable SSH_USER $SSH_USER
 check_env_variable SSH_HOST $SSH_HOST
 check_env_variable PRIVATE_KEY $PRIVATE_KEY
+check_env_variable LOCAL_CIFS_PORT $LOCAL_CIFS_PORT
 
 # restart both autossh and ssh because sometimes autossh does not restart ssh process when its killed
-autossh_pids=$(ps aux | grep "autossh" | grep "$local_port:127.0.0.1:" |  grep -v grep | awk '{print $2}')
+autossh_pids=$(ps aux | grep "autossh" | grep "$remote_cifs_port:127.0.0.1:" |  grep -v grep | awk '{print $2}')
 IFS=$'\n'
 for autossh_pid in $autossh_pids 
 do
@@ -36,15 +36,15 @@ AUTOSSH_PORT=0
 AUTOSSH_GATETIME=0
 # Reduce noise from ssh tunnel errors/restarts etc 
 AUTOSSH_LOGLEVEL=0
-autossh -fNT -o ExitOnForwardFailure=yes -o ServerAliveInterval=10 -o ServerAliveCountMax=3 -i $PRIVATE_KEY -p $SSH_PORT -L $local_port:127.0.0.1:$remote_cifs_port $SSH_USER@$SSH_HOST
+autossh -fNT -o ExitOnForwardFailure=yes -o ServerAliveInterval=10 -o ServerAliveCountMax=3 -i $PRIVATE_KEY -p $SSH_PORT -L $remote_cifs_port:127.0.0.1:$LOCAL_CIFS_PORT $SSH_USER@$SSH_HOST
 
 # check ssh is listening on port. tunnel takes some time to come up
 start_time=$(date +%s)
 max_wait_time_sec=20
 while true; do
-  ssh_tunnel_pid=$(fuser -n tcp $local_port 2>/dev/null| awk -F":" '{print $1}')
+  ssh_tunnel_pid=$(fuser -n tcp $remote_cifs_port 2>/dev/null| awk -F":" '{print $1}')
   if [ -n "$ssh_tunnel_pid" ]; then
-    do_log "ssh tunnel is listening on port $local_port after $elapsed_time seconds"
+    do_log "ssh tunnel is listening on port $remote_cifs_port after $elapsed_time seconds"
     exit 0
   else
     current_time=$(date +%s)
